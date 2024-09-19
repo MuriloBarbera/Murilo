b374fdbb2cea785fda9df31ecb612757e0aca456
MuriloBarbera <murilobardeli@gmail.com>
2024-09-19 10:30:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Função para alternar o menu
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navUl = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        navUl.classList.toggle('active');
+        menuToggle.classList.toggle('open');
+    });
+});
